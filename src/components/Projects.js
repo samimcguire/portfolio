ccbfe73fa8bf87d33c50a2f92dbba8e21769f1c8
Sami McGuire <samimcguire@gmail.com>
2023-12-08 21:36:44 -0700

    refactor projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,16 +5,18 @@ import Row from 'react-bootstrap/Row';
 const Projects = () => {
   return (
     <Row xs={1} md={2} className="g-4">
-      {Array.from({ length: 4 }).map((_, idx) => (
+      {Array.from({ length: 10 }).map((_, idx) => (
         <Col key={idx}>
           <Card>
-            <Card.Img variant="top" src="holder.js/100px160" />
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
+            <Card.Img variant="top" src="PacMan1.png" />
+            <Card.Body class="text-bg-dark me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+              <Card.Title>
+                <h2 class="display-5">Simple Pacman</h2>
+              </Card.Title>
               <Card.Text>
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
+                <p class="lead"><a href="https://samimcguire.github.io/pacman/">Project in Action</a></p>
+                <p class="lead"><a href="https://github.com/samimcguire/pacman">GitHub Repository</a></p>
+                <p id="project-description">In this project, the Pacman bounces inside of the confines of your window.</p>
               </Card.Text>
             </Card.Body>
           </Card>
