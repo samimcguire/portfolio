d7ada79b5f11e6f1b1017a4efb93228744c76e3f
Sami McGuire <samimcguire@gmail.com>
2023-12-09 13:19:48 -0700

    link update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,14 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
   return (
     <Navbar expand="lg" id="navbar-custom">
       <Container>
-        <Navbar.Brand href="/">Sami's Portfolio</Navbar.Brand>
-        <Nav.Link href="/projects">Projects</Nav.Link>
+        <Link to='/' className="nav-link"><li className="nav-item">Sami's Portfolio</li></Link>
+        <Link to='/projects' className="nav-link"><li className="nav-item">Projects</li></Link>
       </Container>
     </Navbar>
   );
